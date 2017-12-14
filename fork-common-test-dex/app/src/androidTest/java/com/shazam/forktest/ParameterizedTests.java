e4120922c44aa00ab56c30f93f89f0406ea7199e
Ivan Balaksha <tagantroy@gmail.com>
2017-12-14 19:12:18 +0800

    add ParameterizedTest

---
+++ fork-common-test-dex/app/src/androidTest/java/com/shazam/forktest/ParameterizedTests.java
@@ -0,0 +1,36 @@
+package com.shazam.annotations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ParameterizedTests {
+
+    String test;
+    int number;
+    String result;
+
+    public ParameterizedTests(String test, int number, String result) {
+        this.test = test;
+        this.number = number;
+        this.result = result;
+    }
+
+    @Test
+    public void sumTestMethod() {
+        assertEquals(test + number, result);
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"test", 1, "test1"}, {"test", 2, "test2"}, {"test", 3, "test3"}
+        });
+    }
+}
