7aff0d2817a967e9dc3b8ee24a2639ed780f9ae9
Ivan Balaksha <tagantroy@gmail.com>
2017-12-19 14:55:07 +0800

    comment debug comparator code

--- fork-runner/src/main/java/com/shazam/fork/batch/TestTaskComparator.kt
+++ fork-runner/src/main/java/com/shazam/fork/batch/TestTaskComparator.kt
@@ -27,8 +27,8 @@ class TestTaskComparator : Comparator<TestTask> {
 
     override fun compare(o1: TestTask, o2: TestTask): Int {
         val res = when {
-            o1 is TestTask.MultiTestTask -> -1
-            o2 is TestTask.MultiTestTask -> 1
+        /*o1 is TestTask.MultiTestTask -> -1
+        o2 is TestTask.MultiTestTask -> 1*/
             else -> {
                 val testMetric1 = extractTestMetric(o1)
                 val testMetric2 = extractTestMetric(o2)
