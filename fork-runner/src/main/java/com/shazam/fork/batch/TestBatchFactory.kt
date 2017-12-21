1792166f06e5769e1c39a9b00963a8bd1633b0c4
Ivan Balaksha <tagantroy@gmail.com>
2017-12-21 17:43:43 +0800

    temporary disable batches

--- fork-runner/src/main/java/com/shazam/fork/batch/TestBatchFactory.kt
+++ fork-runner/src/main/java/com/shazam/fork/batch/TestBatchFactory.kt
@@ -1,30 +1,21 @@
 package com.shazam.fork.batch
 
+import com.agoda.fork.stat.TestMetric
 import com.google.common.collect.Lists
 import com.shazam.fork.batch.tasks.TestTask
 import com.shazam.fork.model.TestCaseEvent
 import java.util.*
+import kotlin.collections.ArrayList
 
 class TestBatchFactory {
+
     fun tasks(list: Collection<TestCaseEvent>): List<TestTask> {
         val average = list.map { it.testMetric.variance }.average()
         val grouped = list.groupBy { it.testMetric.variance < average }
-        val short = grouped[true].orEmpty()
-        val resultArray = ArrayList<TestTask>()
-        val tempArray = ArrayList<TestCaseEvent>()
-        short.forEach {
-            if (tempArray.sumByDouble { it.testMetric.variance } < average*2) {
-                tempArray.add(it)
-            } else {
-                resultArray.add(TestTask.MultiTestTask(Lists.newArrayList(tempArray)))
-                tempArray.clear()
-            }
-        }
-        if(tempArray.isNotEmpty()){
-            resultArray.add(TestTask.MultiTestTask(Lists.newArrayList(tempArray)))
-        }
+        val short = grouped[true].orEmpty().sortedByDescending { it.testMetric.variance }
+
         val single = grouped[false].orEmpty().map { TestTask.SingleTestTask(it) }
 
-        return resultArray + single
+        return short.map { TestTask.SingleTestTask(it) } + single
     }
 }
